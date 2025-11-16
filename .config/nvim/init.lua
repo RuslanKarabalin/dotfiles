a376381176fe957a9e20ababfa5b7c5c06df6dd3
Ruslan Karabalin <karabalin112@gmail.com>
2025-11-16 15:01:52 +0300

    feat: add config for nvim 0.12

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,39 +1,6 @@
-require("config.lazy")
+require("settings")
+require("config.treesitter")
 
-vim.wo.number = true
-vim.wo.relativenumber = true
-
-vim.g.did_load_filetypes = 1
-vim.g.formatoptions = "qrn1"
-vim.opt.showmode = false
-vim.opt.updatetime = 100
-vim.wo.signcolumn = "yes"
-vim.opt.scrolloff = 3
-vim.opt.wrap = false
-vim.wo.linebreak = true
-vim.opt.virtualedit = "block"
-vim.opt.undofile = true
-vim.opt.shell = "/bin/zsh"
-vim.opt.swapfile = false
-
--- Splits
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-
--- Clipboard
-vim.opt.clipboard = "unnamedplus"
-
--- Shorter messages
-vim.opt.shortmess:append("c")
-
--- Indent Settings
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-vim.opt.termguicolors = true
+require("config.completion")
+require("config.lsp")
+require("config.tree")
