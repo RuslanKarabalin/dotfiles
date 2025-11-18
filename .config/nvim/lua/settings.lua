dcf21d4f36f83a8bba8a8b034bca0d16f4ca766b
Ruslan Karabalin <karabalin112@gmail.com>
2025-11-18 20:02:48 +0300

    feat: add plugin for telescope

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -45,6 +45,7 @@ vim.pack.add({
         src = 'https://github.com/nvim-treesitter/nvim-treesitter',
         branch = 'main'
     },
+    { src = 'https://github.com/nvim-lua/plenary.nvim' },
     { src = 'https://github.com/iamcco/markdown-preview.nvim' },
     { src = 'https://github.com/nvim-telescope/telescope.nvim' },
     { src = 'https://github.com/nvim-telescope/telescope-fzf-native.nvim' },
