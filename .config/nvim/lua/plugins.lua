6fed8b7475808dc5cac7a678efce12d23e17de82
Ruslan Karabalin <karabalin112@gmail.com>
2025-12-19 18:39:30 +0300

    feat: simplify nvim config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -5,10 +5,7 @@ vim.pack.add({
     { src = 'https://github.com/hrsh7th/cmp-nvim-lsp' },
     { src = 'https://github.com/nvim-tree/nvim-tree.lua' },
     { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
-    {
-        src = 'https://github.com/nvim-treesitter/nvim-treesitter',
-        branch = 'main'
-    },
+    { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
     { src = 'https://github.com/nvim-lua/plenary.nvim' },
     { src = 'https://github.com/iamcco/markdown-preview.nvim' },
     { src = 'https://github.com/nvim-telescope/telescope.nvim' },
--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -1,27 +1,10 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
 vim.wo.number = true
 vim.wo.relativenumber = true
-
-vim.g.formatoptions = "qrn1"
-vim.opt.showmode = false
-vim.opt.updatetime = 100
-vim.wo.signcolumn = "yes"
-vim.opt.scrolloff = 3
-vim.opt.wrap = false
 vim.wo.linebreak = true
-vim.opt.virtualedit = "block"
-vim.opt.undofile = true
-vim.opt.shell = "/bin/zsh"
-vim.opt.swapfile = false
 
+vim.opt.hlsearch = false
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-
-vim.opt.clipboard = "unnamedplus"
-
-vim.opt.shortmess:append("c")
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
@@ -29,14 +12,9 @@ vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+vim.opt.background = 'dark'
+vim.opt.termguicolors = true
+vim.cmd.colorscheme('gruvbox-material')
 
 vim.opt.colorcolumn = "80,120"
 vim.cmd [[highlight ColorColumn ctermbg=lightgrey guibg=lightgrey]]
-
-vim.opt.termguicolors = true
-vim.cmd.colorscheme('gruvbox-material')
-vim.opt.background = 'dark'
-
-vim.opt.hlsearch = false
