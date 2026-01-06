af5654e280432bb42c1a01347e4077403c8611ce
Ruslan Karabalin <karabalin112@gmail.com>
2026-01-06 13:27:15 +0300

    fix: add deps

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,5 +2,7 @@ vim.pack.add({
     { src = 'https://github.com/sainnhe/gruvbox-material' },
     { src = 'https://github.com/nvim-tree/nvim-tree.lua' },
     { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
+    { src = 'https://github.com/MunifTanjim/nui.nvim' },
+    { src = 'https://github.com/folke/noice.nvim' },
     { src = 'https://github.com/iamcco/markdown-preview.nvim' },
 })
